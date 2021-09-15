4e325607a34d77bb33a038afb0cc64901588e02d
jelenapetrovacki <jpetrovacki3@gmail.com>
2021-09-15 13:54:10 +0200

    konstante

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,4 @@
-export const FORMAA_URL = 'https://catastral-data-app.herokuapp.com/formaa';
-export const FORMAB_URL = 'https://catastral-data-app.herokuapp.com/formab';
-export const KORISNIK_ID_URL = 'https://catastral-data-app.herokuapp.com/korisnik';
-export const STAROSNAGRUPA_ID_URL = 'https://catastral-data-app.herokuapp.com/starosnaGrupa';
+export const FORMAA_URL = 'https://katastarski-podaci.herokuapp.com/formaa';
+export const FORMAB_URL = 'https://katastarski-podaci.herokuapp.com/formab';
+export const KORISNIK_ID_URL = 'https://katastarski-podaci.herokuapp.com/korisnik';
+export const STAROSNAGRUPA_ID_URL = 'https://katastarski-podaci.herokuapp.com/starosnaGrupa';
